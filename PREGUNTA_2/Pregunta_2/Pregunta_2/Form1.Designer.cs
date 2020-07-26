20607e936bf3e2e02412165902f3e153c45e84e7
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-07-25 23:30:36 -0400

    Respuesta a la Pregunta 2

---
+++ PREGUNTA_2/Pregunta_2/Pregunta_2/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Pregunta_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.contorno = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contorno
+            // 
+            this.contorno.Location = new System.Drawing.Point(742, 105);
+            this.contorno.Name = "contorno";
+            this.contorno.Size = new System.Drawing.Size(131, 23);
+            this.contorno.TabIndex = 5;
+            this.contorno.Text = "Contorno";
+            this.contorno.UseVisualStyleBackColor = true;
+            this.contorno.Click += new System.EventHandler(this.contorno_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(742, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Cargar imagen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(620, 490);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 602);
+            this.Controls.Add(this.contorno);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button contorno;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ PREGUNTA_2/Pregunta_2/Pregunta_2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pregunta_2
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Color c1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Carga una imagen y la muestra desde un PictureBox
+            openFileDialog1.Filter = "Archivos JPG |*.jpg|Archivos BMP|*.bmp|Todos los archivos|*.*";
+            openFileDialog1.ShowDialog();
+            bmp = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bmp;
+        }
+
+        private void contorno_Click(object sender, EventArgs e)
+        {
+            // Aplicacion filtro Laplaciano de 3x3 para obtener el contorno
+            // [[0,1,0]
+            // [1,-4,1]
+            // [0,1,0]]
+            Bitmap bmp1 = new Bitmap(bmp.Width,bmp.Height);
+            int width = bmp.Width - 1, height = bmp.Height - 1;
+            for (int i = 1; i < width; i++)
+            {
+                for (int j = 1; j < height; j++)
+                {
+                    Color a, b, c, d,e1;
+                    a = bmp.GetPixel(i, j - 1);
+                    b = bmp.GetPixel(i - 1, j);
+                    c = bmp.GetPixel(i, j);
+                    d = bmp.GetPixel(i + 1, j);
+                    e1 = bmp.GetPixel(i, j + 1);
+                    int red = a.R + b.R + c.R * (-4) + d.R + e1.R;
+                    int green = a.G + b.G + c.G * (-4) + d.G + e1.G;
+                    int blue = a.B + b.B + c.B * (-4) + d.B + e1.B;
+                    int media = (red + green + blue) / 3;
+                    if (media > 255) media = 255;
+                    if (media < 0) media = 0;
+                    bmp1.SetPixel(i,j,Color.FromArgb(media,media,media));
+                }                              
+            }
+            pictureBox1.Image = bmp1;                 
+        }                                  
+      }
+}
